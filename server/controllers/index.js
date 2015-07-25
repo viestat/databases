087088c7d66941ce37ae310cca7de196fb031b13
viestat <viestat@icloud.com>
2015-07-24 21:27:52 -0700

    write the next great american novel

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,32 +1,67 @@
 var models = require('../models');
 var bluebird = require('bluebird');
-var db = require('../db')
+var db = require('../db');
 
 
 
+// /classes/messages
 module.exports = {
+  // options: {
+  //   optionsresponse: function(){ console.log('options access-control-allow-methods') }
+  // },
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get(function(err, results){});
+
+      res.writeHead(200, headers);
+      res.end('GET IT');
+    }, 
+
     post: function (req, res) {
+      var mesageBody = {
+        message: req.body.message,
+      };
 
-      var message = {
-        uid: 2,
-        rid: 3,
-        message: 'HEy!'
-      }
-
-      var query  = db.query('INSERT INTO messages SET ?', message, function (err, result){
-        if(err){ 
-          res.writeHead(400, headers);
-          res.end('Failed to POST data. error:' + err); 
-        }  
-        res.writeHead(201, headers);
-        res.end('POST Success!');
+      // if models.users.get(username) is not null
+          // add uid of usernmae to MessageBody
+      // else models.users.post(username)
+          // add new uid to MessageBody
+      models.users.get(req.body.username, function(err, result){
+        if(err){ console.log(err); return; }
+        if(result.length === 0){
+          models.users.post(req.body.username, function(err, result){
+            if(result){
+              
+            }
+          });
+        }else{
+          result
+        }
       });
-    } // a function which handles posting a message to the database
+
+      models.rooms.get(req.body.roomname, function(err, result){});
+
+      // models.messages.post(req.body, function(err, result){
+      //   if(err){
+      //     res.writeHead(400, headers);
+      //     res.end('Failed to POST data. error:' + err); 
+      //   }else{
+      //     res.writeHead(201, headers);
+      //     res.end('POST Success! Result: ', result);
+      //   }
+      // });
+    }
   },
 
   users: {
+    // Ditto as above
+    get: function (req, res) {
+      models.users.get(req.body.username, function(err, result){});
+    },
+    post: function (req, res) {}
+  },
+
+  rooms: {
     // Ditto as above
     get: function (req, res) {},
     post: function (req, res) {}
--- server/models/index.js
+++ server/models/index.js
@@ -5,14 +5,42 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.query('SELECT * FROM messages', undefined, function(err, result){
+        callback(err, result);
+      });
+    },
+    post: function (data, callback) {
+      var query  = db.query('INSERT INTO messages SET ?', data, function (err, result){
+        callback(err, result);
+      });
+    }
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (username, callback) {
+      return db.query('SELECT id FROM users WHERE username = ?', username, function (err, result){
+        callback(err, result);
+      });
+
+    },
+    post: function (username, callback) {
+      var query  = db.query('INSERT INTO users SET ?', {'username': username}, function (err, result){
+        // result
+          // { fieldCount: 0,
+          //   affectedRows: 1,
+          //   insertId: 2,
+          //   serverStatus: 2,
+          //   warningCount: 0,
+          //   message: '',
+          //   protocol41: true,
+          //   changedRows: 0 }
+
+        callback(err, result);
+
+      });
+    }
   }
 };
 
